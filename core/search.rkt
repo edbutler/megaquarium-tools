21241a9be346f054bfb383998fc9f2ce974a89e7
edbutler <eric@enlearn.org>
2020-06-07 07:57:35 -0700

    add test

--- core/search.rkt
+++ core/search.rkt
@@ -14,3 +14,17 @@
 
 (define (fuzzy-match-species search-str lst)
   (fuzzy-match-search (compose1 symbol->string species-id) search-str lst))
+
+(module+ test
+  (require rackunit)
+
+  (test-case "search str allows dropping leading id number"
+    (define lst '("2_crescent_earthen" "4_pancake_scuppernong" "7_violet_crescent"))
+    (check-equal? (fuzzy-match-search identity "pancake" lst) (list (second lst)))
+    (check-equal? (fuzzy-match-search identity "olet" lst) (list (third lst)))
+    (check-equal? (fuzzy-match-search identity "then" lst) (list (first lst)))
+    (check-equal? (fuzzy-match-search identity "cresc" lst) (list (first lst) (third lst)))
+    (check-equal? (fuzzy-match-search identity "xyz" lst) empty))
+
+
+)
