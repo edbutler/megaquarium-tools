52a6e07b7a5e51b59e07e00a6e13fae27adcea49
Eric Butler <edbutler@users.noreply.github.com>
2023-07-28 17:03:21 -0700

    more progress

--- src/sexp.rs
+++ src/sexp.rs
@@ -14,11 +14,19 @@ impl SExp {
     pub fn keyword<S: Into<String>>(kw: S, arg: SExp) -> SExp {
         SExp::Keyword(kw.into(), Box::new(arg))
     }
+}
 
+fn print(exp: &SExp, f: &mut std::fmt::Formatter, indent: u32) -> std::fmt::Result {
+    match exp {
+        SExp::Int(i) => write!(f, "{}", i)?,
+        _ => ()
+    };
+    Ok(())
 }
 
 impl std::fmt::Display for SExp {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        print(self, f, 0);
         Ok(())
     }
 }
